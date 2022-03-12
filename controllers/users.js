45af74285b04e37840d8890faf5b7f60e01ab83d
Gerimae Oliva <gerimae.o@gmail.com>
2022-03-12 11:04:00 -0500

    updated Signup and Login routers in server.js

--- controllers/users.js
+++ controllers/users.js
@@ -12,13 +12,41 @@ router.get('/signup', (req, res) => {
     res.render('user/Signup.jsx')
 });
 
-router.post('/signup', (req, res) => {
-    res.send('signup')
+router.post('/signup', async (req, res) => {
+    req.body.password = await bcrypt.hash(
+        req.body.password,
+        await bcrypt.genSalt(10)
+    );
+    User.create(req.body)
+        .then((user) => {
+            res.redirect('/user/login');
+        })
+        .catch((err) => {
+            console.log(err)
+            res.json({ err });
+        })
 });
 
 // Login Routes
-router.get('/login', (req, res) => {
-    res.render('user/Login.jsx')
+router.get('/login', async (req, res) => {
+    const { username, password } = req.body;
+    User.findOne({ username })
+        .then(async (user) => {
+            if(user){
+                const result = await bcrypt.compare(password, user.password);
+                if(result){
+                    res.redirect('/bills');
+                } else {
+                    res.json({ error: "Password Does Not Match"});
+                }
+            } else {
+                res.json({ error: "User Does Not Exist"});
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+            res.json({ err });
+        })
 });
 
 router.post('/login', (req, res) => {
