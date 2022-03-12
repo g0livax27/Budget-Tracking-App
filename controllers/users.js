e9a50aa192aeee90bffbf1a222236d2171cc193c
Gerimae Oliva <gerimae.o@gmail.com>
2022-03-12 11:41:31 -0500

    Added a logout route to end session

--- controllers/users.js
+++ controllers/users.js
@@ -72,4 +72,10 @@ router.post('/login', async (req, res) => {
         })
 });
 
+router.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        res.redirect('/');
+    })
+});
+
 module.exports = router;
