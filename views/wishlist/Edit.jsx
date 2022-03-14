aab01aaa7a240dd70b811bcbb06478606ef7cda1
Gerimae Oliva <gerimae.o@gmail.com>
2022-03-14 15:11:26 -0400

    Editing wishlist/Edit.jsx file to match bills/Edit.jsx file

--- views/wishlist/Edit.jsx
+++ views/wishlist/Edit.jsx
@@ -6,30 +6,32 @@ class Edit extends React.Component {
         const { wishlist } = this.props;
         return(
             <Default>
-                <form class="edit" action={`/wishlist/${wishlist._id}?_method=PUT`} method="POST">
-                    <fieldset>
-                        <legend>{`Edit ${wishlist.itemName}`}</legend>
-                        <label>
-                            Item Name:<input
-                            type="text"
-                            name="itemlName"
-                            placeholder="enter item name"
-                                defaultValue={wishlist.itemName}/>
-                        </label><br/>
-                        <label>
-                            Item Amount:<input
-                            type="text"
-                            name="itemAmount"
-                            placeholder="enter item amount"
-                                defaultValue={wishlist.itemAmount}/>
-                        </label><br/>
-                        <label>
-                            A Must Have?:{ this.props.wishlist.mustHave? <input type="checkbox" name="mustHave" defaultChecked />: <input type="checkbox" name="mustHave"/> }
-                        </label>
-                    </fieldset>
-                    <input type="submit" value="Submit Change"/><br/>
-                    <a href="/wishlist">Back to Main</a>
-                </form>
+                <div>
+                    <form class="edit" action={`/wishlist/${wishlist._id}?_method=PUT`} method="POST">
+                        <fieldset>
+                            <legend>{`Edit ${wishlist.itemName}`}</legend>
+                            <label>
+                                Item Name:<input
+                                type="text"
+                                name="itemlName"
+                                placeholder="enter item name"
+                                    defaultValue={wishlist.itemName}/>
+                            </label><br/>
+                            <label>
+                                Item Amount:<input
+                                type="text"
+                                name="itemAmount"
+                                placeholder="enter item amount"
+                                    defaultValue={wishlist.itemAmount}/>
+                            </label><br/>
+                            <label>
+                                A Must Have?:{ this.props.wishlist.mustHave? <input type="checkbox" name="mustHave" defaultChecked />: <input type="checkbox" name="mustHave"/> }
+                            </label>
+                        </fieldset>
+                        <input type="submit" value="Submit Change"/><br/>
+                        <a href="/wishlist">Back to Monthly Wish Lists</a>
+                    </form>
+                </div>
             </Default>
         )
     }
